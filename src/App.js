65b3674c6bc30f0c32fcebf1fa67ec2d74d067a2
batouche-dev <azedine.batouche@outlook.com>
2021-05-06 16:20:55 +0200

    feature: add Feed Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/SideBar';
+import Feed from './components/feed/Feed';
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
       <Header />
 
       <div className='app_body'>
+        <div className='columSpace'></div>
         <Sidebar />
+        <Feed />
+        <div className='columSpace'></div>
       </div>
     </div>
   );
